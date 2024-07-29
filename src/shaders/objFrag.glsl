c116360ca831ebcdba6210835e0e66e93409ea8c
Nick-Petruccelli <nickpetro18@gmail.com>
2024-07-28 20:44:03 -0400

    Lighting: Specular reflections

--- src/shaders/objFrag.glsl
+++ src/shaders/objFrag.glsl
@@ -1,18 +1,27 @@
 #version 330 core
 in vec3 normal;
 in vec3 fragPos;
+in vec3 LightPos;
 
-uniform vec3 lightPos;
 
 out vec4 FragColor;
 void main(){
-    vec3 objColor = vec3(0.8, 0.5, 0.1);
+    vec3 objColor = vec3(0.6, 0.2, 0.1);
     vec3 lightColor = vec3(1.0);
+
     vec3 ambient = 0.1 * lightColor;
+
     vec3 norm = normalize(normal);
-    vec3 lightDir = normalize(lightPos - fragPos);
+    vec3 lightDir = normalize(LightPos - fragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
-    vec3 sum = (ambient + diffuse) * objColor;
+
+    float specularStrength = .9;
+    vec3 viewDir = normalize(-fragPos);
+    vec3 lightRef = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, lightRef), 0.0), 128);
+    vec3 specular = specularStrength * spec * lightColor;
+
+    vec3 sum = (ambient + diffuse + specular) * objColor;
     FragColor = vec4(sum, 1.0); 
 }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -5,12 +5,15 @@ layout (location = 1) in vec3 aNorm;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 proj;
+uniform vec3 lightPos;
 
 out vec3 normal;
 out vec3 fragPos;
+out vec3 LightPos;
 void main()
 {
-    normal = mat3(transpose(inverse(model))) * aNorm;
-    fragPos = vec3(model * vec4(aPos, 1.0));
+    normal = mat3(transpose(inverse(view * model))) * aNorm;
+    fragPos = vec3(view * model * vec4(aPos, 1.0));
+    LightPos = vec3(view * vec4(lightPos, 1.0));
     gl_Position = proj * view * model * vec4(aPos.x, aPos.y, aPos.z, 1.0);
 }
